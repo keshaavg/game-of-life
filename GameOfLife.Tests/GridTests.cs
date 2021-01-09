9f53edf542044ece24cd5b230355ddcb6440eba6
Keshav Goel <keshavgoel.in@gmail.com>
2021-01-09 21:38:40 +0000

    Added edge cases

--- GameOfLife.Tests/GridTests.cs
+++ GameOfLife.Tests/GridTests.cs
@@ -24,5 +24,43 @@ namespace GameOfLife.Tests
             Assert.Equal("...**...", gen2[2]);
             Assert.Equal("........", gen2[3]);
         }
+
+        [Fact]
+        public void WhenGridRunsGenaration2WithEdgeCase1_GetExpectedResult()
+        {
+            var gen1 = new string[]
+                {
+                    "*......*",
+                    "....*...",
+                    "...**...",
+                    "*......*"
+                };
+
+            var gen2 = new Grid().NextGeneration(gen1, 4, 8);
+            Assert.True(gen2.Count() == 4);
+            Assert.Equal("........", gen2[0]);
+            Assert.Equal("...**...", gen2[1]);
+            Assert.Equal("...**...", gen2[2]);
+            Assert.Equal("........", gen2[3]);
+        }
+
+        [Fact]
+        public void WhenGridRunsGenaration2WithEdgeCase2_GetExpectedResult()
+        {
+            var gen1 = new string[]
+                {
+                    "........",
+                    "..***...",
+                    "...**...",
+                    "........"
+                };
+
+            var gen2 = new Grid().NextGeneration(gen1, 4, 8);
+            Assert.True(gen2.Count() == 4);
+            Assert.Equal("...**...", gen2[0]);
+            Assert.Equal("..*.....", gen2[1]);
+            Assert.Equal("...*....", gen2[2]);
+            Assert.Equal("........", gen2[3]);
+        }
     }
 }
